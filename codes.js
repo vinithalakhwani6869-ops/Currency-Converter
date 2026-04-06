08465d7bb0920537c00c8bf24b620a4b5450f484
Vinitha <vinithalakhwani6869@gmail.com>
2026-04-06 12:11:59 +0530

    Initial commit - Currency Converter

---
+++ codes.js
@@ -0,0 +1,247 @@
+const amountInput = document.getElementById("amount");
+const fromCurrency = document.getElementById("fromCurrency");
+const toCurrency = document.getElementById("toCurrency");
+const fromFlag = document.getElementById("fromFlag");
+const toFlag = document.getElementById("toFlag");
+const button = document.getElementById("btn");
+const result = document.getElementById("result");
+
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+//creating dropdowns
+for (let currrency in countryList) {
+   let option1 = document.createElement("option");
+   option1.value = currrency;
+   option1.innerText = currrency;
+   fromCurrency.append(option1);
+
+   let option2 = document.createElement("option");
+   option2.value = currrency;
+   option2.innerText = currrency;
+   toCurrency.append(option2);
+}
+// default values
+fromCurrency.value = "USD";
+toCurrency.value = "INR";
+// updating flags with user-choice
+fromCurrency.addEventListener("change",()=>{
+  const code = countryList[fromCurrency.value];
+  fromFlag.src = `https://flagsapi.com/${code}/flat/64.png`;
+});
+
+toCurrency.addEventListener("change",()=>{
+  const code = countryList[toCurrency.value];
+  toFlag.src = `https://flagsapi.com/${code}/flat/64.png`;
+});
+
+button.addEventListener("click",convertCurrency());
+
+async function convertCurrency() {
+  const amount = amountInput.value;
+  const from = fromCurrency.value;
+  const to = toCurrency.value;
+
+  if(amount === "" || amount<=0){
+    result.innerText = "Please enter a valid amount";
+    return;
+  }
+
+  try{
+    const response = await fetch(
+      `https://open.er-api.com/v6/latest/${from}`
+    );
+
+    const data = await response.json();
+    const rate = data.rates[to];
+    const convertedAmount = (amount*rate).toFixed(2);
+
+    result.innerText = `${amount} ${from} = ${convertedAmount} ${to}`;
+  }
+    catch(error){
+      result.innerText = "Something went wrong";
+      console.error(error);
+  }
+}
+  
+
+button.addEventListener("click", (e) => {
+  e.preventDefault(); // ⛔ stops form reload
+  convertCurrency();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
